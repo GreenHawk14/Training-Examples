09b5949e7df4ac08d3247d6a430ada30b802707b
Lennard Mazyck Jr <lennymazyck21@outlook.com>
2020-11-16 21:26:44 -0500

    Arrange class for binding School information.

--- NYCSchoolsMVP/app/src/main/java/com/example/nycschoolsmvp/model/SchoolAdapter.kt
+++ NYCSchoolsMVP/app/src/main/java/com/example/nycschoolsmvp/model/SchoolAdapter.kt
@@ -9,9 +9,12 @@ class SchoolAdapter (val SchoolList: ArrayList<NYCListSchoolResponse> )
 {
     class SchoolViewHolder(val schoolView: View): RecyclerView.ViewHolder(schoolView)
     {
-        val schoolName = schoolView.findViewById(R.id.tv_item_school_name) as TextView
-        val schoolAddress = schoolView.findViewById(R.id.tv_school_address) as TextView
-        val schoolNum = schoolView.findViewById(R.id.tv_school_phone) as TextView
+        fun bindSchoolItems()
+        {
+            val schoolName = schoolView.findViewById(R.id.tv_item_school_name) as TextView
+            val schoolAddress = schoolView.findViewById(R.id.tv_school_address) as TextView
+            val schoolNum = schoolView.findViewById(R.id.tv_school_phone) as TextView
+        }
     }
 
 }
